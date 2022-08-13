e0abf2af2ea66d4df28561218e912870871938c0
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 16:50:33 +0600

    #3 Добавил сервис для создания темы

---
+++ exam8/Services/Abstractions/ITopicServicecs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using exam8.Models;
+using exam8.ViewModels;
+
+namespace exam8.Services.Abstractions
+{
+    public interface ITopicService
+    {
+        public void CreateTopic(TopicViewModel model);
+        public IEnumerable<Topic> GetTopics();
+        public Topic GetTopic(int id);
+    }
+}
---
+++ exam8/Services/TopicService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using exam8.Helpers;
+using exam8.Models;
+using exam8.Services.Abstractions;
+using exam8.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace exam8.Services
+{
+    public class TopicService : ITopicService
+    {
+        private readonly ForumContext _context;
+
+        public TopicService(ForumContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateTopic(TopicViewModel model)
+        {
+            Topic topic = model.MapToTopic();
+            if (topic != null)
+            {
+               _context.Topics.Add(topic); 
+               _context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Topic> GetTopics()
+        {
+            var topics = _context.Topics.Include(t => t.User).Include(t => t.Answers).AsEnumerable();
+            return topics;
+        }
+
+        public Topic GetTopic(int id)
+        {
+            Topic topic = _context.Topics
+                .Include(t => t.User)
+                .Include(t => t.Answers)
+                .FirstOrDefault(t => t.Id == id);
+            return topic;
+
+        }
+    }
+}
