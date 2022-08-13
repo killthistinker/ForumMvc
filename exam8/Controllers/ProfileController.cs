6afd672ae09ae07c178be5fe23fd9dc2b6d73262
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 16:53:46 +0600

    Добавил котнроллер для профиля

---
+++ exam8/Controllers/ProfileController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using exam8.Models;
+using exam8.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exam8.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly IProfileService _profileService;
+       
+
+        public ProfileController(IProfileService profileService)
+        {
+            _profileService = profileService;
+          
+        }
+
+        public async Task<IActionResult> Index(string userName)
+        {
+            User user = await _profileService.GetUser(userName);
+            if (user is null)
+            {
+                return RedirectToAction("Error", "Errors", new {statusCode = 404});
+            }
+            return View(user);
+        }
+    }
+}
