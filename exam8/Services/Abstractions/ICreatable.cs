80da47b10e2628889d21db6b99051cccfd17f43d
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 11:55:45 +0600

    #2 Добавил обощеный интерфейс

---
+++ exam8/Services/Abstractions/ICreatable.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace exam8.Services.Abstractions
+{
+    public interface  ICreatable<in T> where T : class
+    {
+        public Task CreateAsync(T entity, string userName);
+    }
+}
