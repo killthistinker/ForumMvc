07d1c642e51272f690a795dbbfab0863b3e4bd7b
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 16:50:08 +0600

    #3 Добавил необходимые модели

---
+++ exam8/Models/Topic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exam8.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreate { get; set; }
+        public List<Answer> Answers { get; set; }
+
+        public Topic()
+        {
+            Answers = new List<Answer>();
+        }
+    }
+}
