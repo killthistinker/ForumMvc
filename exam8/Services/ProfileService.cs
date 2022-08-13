2111e20dead2fb3358b5696e0823846c430bc0b9
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 16:55:20 +0600

    Добавил сервисы для поиска пользователя

---
+++ exam8/Services/ProfileService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using exam8.Models;
+using exam8.Services.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace exam8.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ProfileService(UserManager<User> userManager)
+        {
+           
+            _userManager = userManager;
+        }
+
+        public async Task<User> GetUser(string name)
+        {
+            User user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == name);
+            if (user is null) return null;
+            return user;
+        }
+    }
+}
