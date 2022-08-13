2774c6018f59a56c336885b9e900a161e04ed2c4
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 11:50:14 +0600

    #1 Создал проект подключил необходимые инструменты

---
+++ exam8/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using exam8.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace exam8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                StatusCode = 0,
+                Title = null,
+                Message = null
+            });
+        }
+    }
+}
