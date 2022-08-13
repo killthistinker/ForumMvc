bf31c1e5dc18744ffec8a6f25c93900a66ec651d
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 12:10:58 +0600

    #2 Добавил скрипты для показа пароля

--- exam8/wwwroot/js/site.js
+++ exam8/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#password-view").on("click", function () {
+        var type = $("#password").attr('type') === "text" ? "password" : "text";
+        $("#password").prop('type', type);
+    });
+
+    $("#confirm-view").on("click", function () {
+        var type = $("#confirm").attr('type') == "text" ? "password" : "text";
+        $("#confirm").prop('type', type);
+    });
+    
+    $("#password-view-login").on("click", function () {
+        var type = $("#login-password").attr('type') == "text" ? "password" : "text";
+        $("#login-password").prop('type', type);
+    });
+})
