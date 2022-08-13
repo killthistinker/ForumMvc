a739b954cd603056fc62bd6e1c75b6c1682455fe
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 16:51:05 +0600

    #3 Обновил миграции

--- exam8/Migrations/20220813081403_AnswersAndTopics.Designer.cs
+++ exam8/Migrations/20220813081403_AnswersAndTopics.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using exam8.Models;
@@ -9,9 +10,10 @@ using exam8.Models;
 namespace exam8.Migrations
 {
     [DbContext(typeof(ForumContext))]
-    partial class ForumContextModelSnapshot : ModelSnapshot
+    [Migration("20220813081403_AnswersAndTopics")]
+    partial class AnswersAndTopics
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
