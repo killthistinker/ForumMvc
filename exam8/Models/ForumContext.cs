5c296bef4dcdcd8f14c14cc4eb26c716530574ab
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 11:58:17 +0600

    #2 Добавил контекст

---
+++ exam8/Models/ForumContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace exam8.Models
+{
+    public class ForumContext : IdentityDbContext<User,Role,int>
+    {
+        
+        public ForumContext(DbContextOptions<ForumContext> options) : base(options)
+        {
+            
+        }
+    }
+}
