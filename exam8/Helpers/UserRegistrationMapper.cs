0428ece89cb686aa1be2a6b16cf3b2be7dc84495
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 11:56:27 +0600

    #2 Добавил маппер для модели User

---
+++ exam8/Helpers/UserRegistrationMapper.cs
@@ -0,0 +1,19 @@
+﻿using exam8.Models;
+using exam8.ViewModels;
+
+namespace exam8.Helpers
+{
+    public static class UserRegistrationMapper
+    {
+        public static User MapToUser(this RegisterViewModel self)
+        {
+            User user = new User
+            {
+                Email = self.Email,
+                UserName = self.UserName,
+                Avatar = self.ImagePath,
+            };
+            return user;
+        }
+    }
+}
