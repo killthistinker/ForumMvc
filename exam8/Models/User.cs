53f9cf6ae5f735c4fb9425c266d65def912790fc
Maxim Popovich <killthistinker@gmail.com>
2022-08-13 11:53:29 +0600

    #2 Добавил модель для пользователя

---
+++ exam8/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace exam8.Models
+{
+    public class User : IdentityUser<int>
+    { 
+        public string Avatar { get; set; }
+    }
+}
